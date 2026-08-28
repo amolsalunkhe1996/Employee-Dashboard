b350fd0abb696fe4618119f1840e4a06c15ce889
Amol Salunkhe <amolsalunkhe1996@gmail.com>
2026-08-28 21:26:30 +0530

    Add employee management services and user modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './user/employee-list/employee-list.component';
+import { AddEmployeeComponent } from './user/add-employee/add-employee.component';
+import { ViewEmployeeComponent } from './user/view-employee/view-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    AddEmployeeComponent,
+    ViewEmployeeComponent
   ],
   imports: [
     BrowserModule,
